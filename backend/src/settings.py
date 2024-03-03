9aad88b6a921b6ad1020c42cd9372d13d71b67c0
Will Sams <will@samswebs.com>
2024-03-03 10:43:42 -0500

    Update settings.py

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -2,17 +2,8 @@ from os import getenv
 
 API_NAME = "Acme Hotel Reservation - Graphql API"
 API_PORT = getenv("RESERVATION_PORT") or 80
-ENV = getenv("ENV") or "development"
-
-DB_HOST = getenv("PG_HOST") or "localhost"
-DB_PORT = getenv("PG_PORT") or 5432
-DB_USER = getenv("PG_USER") or "postgres"
-DB_PASSWD = getenv("PG_PASSWD") or getenv("password") or "postgres"
-DB_NAME = getenv("PG_NAME") or f"hotel_db_${ENV}"
-DB_URL = (
-    getenv("PG_URL")
-    or f"postgresql://${DB_USER}:${DB_PASSWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
-)
+ENV = getenv("ENV")
+DB_URL = getenv("PG_URL")
 
 IS_DEBUG = bool(int(getenv("IS_DEBUG", "0"))) or False
 
